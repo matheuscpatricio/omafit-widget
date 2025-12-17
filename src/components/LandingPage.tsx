0924508000766d0982bc6203d878a98ebf12fc35
João Henrique Soares <139515449+jotagadev@users.noreply.github.com>
2025-12-17 11:52:22 -0300

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -368,7 +368,7 @@ export function LandingPage({ onGetStarted, onLogin }: LandingPageProps) {
               className="md:hidden w-full h-full object-cover"
             >
               <source
-                src="https://lhkgnirolvbmomeduoaj.supabase.co/storage/v1/object/public/Video%20banner/videolandingpage.mov"
+                src="https://lhkgnirolvbmomeduoaj.supabase.co/storage/v1/object/public/Video%20banner/fucionalidades.mp4"
                 type="video/mp4"
               />
             </video>
