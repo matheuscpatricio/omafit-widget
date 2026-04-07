2cc5527f36260a92dd9a761f7b753eaff130c2b7
matheuscpatricio <mpatricio621@gmail.com>
2026-04-07 11:17:01 -0300

    (

--- public/omafit-ar-widget.js
+++ public/omafit-ar-widget.js
@@ -647,7 +647,7 @@ async function runArSession({
     const modelFix = new THREE.Group();
     modelFix.rotation.order = "YXZ";
     modelFix.rotation.set(
-      readRotRad("arGlbRotX", 78),
+      readRotRad("arGlbRotX", 84),
       readRotRad("arGlbRotY", 90),
       readRotRad("arGlbRotZ", 0),
     );
